4f8ef234fdcecb4c042723ea8107bfb800173566
Anis Benmakhlouf <anis.benmakhlouf@progedsolutions.com>
2022-12-18 21:38:36 +0100

    First commit

---
+++ Enseignant/src/main/java/java/tn/iit/entity/User.java
@@ -0,0 +1,27 @@
+package java.tn.iit.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "AgentDeTirage")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "street")
+	private String street;
+
+	@Column(name = "city")
+	private String city;
+	
+	@Column(name = "Id_Ens")
+	private String Id_Ens;
+}
---
+++ demo/src/main/java/tn/iit/glid3/DemoApplication.java
@@ -0,0 +1,14 @@
+package tn.iit.glid3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
