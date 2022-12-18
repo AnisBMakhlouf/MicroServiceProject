5c48c885d70f01b3ccace3729bb89c818ba9309d
Nesrine <masmoudinesrine94@gmail.com>
2022-12-18 23:07:03 +0100

    CommitImpressionService

--- Impression-MicroService/src/main/java/tn/iit/entity/Impression.java
+++ Impression-MicroService/src/main/java/tn/iit/entity/Impression.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Utilisateurs")
+@Table(name = "Impression")
 public class Impression {
 
 	@Id
@@ -34,6 +34,70 @@ public class Impression {
 	private Long Id_Ens;
 	
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getNbrCopie() {
+		return NbrCopie;
+	}
+
+	public void setNbrCopie(int nbrCopie) {
+		NbrCopie = nbrCopie;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	public String getDate() {
+		return Date;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
+	public String getFile() {
+		return File;
+	}
+
+	public void setFile(String file) {
+		File = file;
+	}
+
+	public Long getId_Ens() {
+		return Id_Ens;
+	}
+
+	public void setId_Ens(Long id_Ens) {
+		Id_Ens = id_Ens;
+	}
+
+	public Long getId_Group() {
+		return Id_Group;
+	}
+
+	public void setId_Group(Long id_Group) {
+		Id_Group = id_Group;
+	}
+
+	public Long getId_Subject() {
+		return Id_Subject;
+	}
+
+	public void setId_Subject(Long id_Subject) {
+		Id_Subject = id_Subject;
+	}
+
 	@Column(name = "Id_Group")
 	private Long Id_Group;
 	
---
+++ Impression-MicroService/src/main/java/tn/iit/response/ImpressionResponse.java
@@ -0,0 +1,5 @@
+package tn.iit.response;
+
+public class ImpressionResponse {
+
+}
