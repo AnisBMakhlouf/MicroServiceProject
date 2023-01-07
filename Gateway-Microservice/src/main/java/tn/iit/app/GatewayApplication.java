2297603b33d8c304a6420db82a7f4f785e0836cc
Anis Benmakhlouf <anis.benmakhlouf@progedsolutions.com>
2023-01-07 14:29:25 +0100

    gateway main

--- Gateway-Microservice/src/main/java/tn/iit/app/GatewayApplication.java
+++ Gateway-Microservice/src/main/java/tn/iit/app/GatewayApplication.java
@@ -1,5 +1,20 @@
 package tn.iit.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EnableEurekaClient
+@SpringBootApplication
+@ComponentScan({"tn.iit.controller", "tn.iit.service"})
+@EntityScan("tn.iit.entity")
+@EnableJpaRepositories("tn.iit.repository")
 public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
 
 }
